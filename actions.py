cb0ac3a73f57bbc25387fdd9ac2214e93582488d
Callan Horton <cgahorton@gmail.com>
2024-06-25 15:40:11 +0930

    added input_handlers and actions

---
+++ actions.py
@@ -0,0 +1,15 @@
+class Action:
+    pass
+
+
+class EscapeAction(Action):
+    pass
+
+
+class MovementAction(Action):
+    def __init__(self, dx: int, dy: int):
+        super().__init__()
+        
+        
+        self.dx = dx
+        self.dy = dy
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
     
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+    
     tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
     
     with tcod.context.new_terminal(
@@ -17,17 +20,13 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
             
             context.present(root_console)
             
             for event in tcod.event.wait():
                 if event.type == "QUIT":
                     raise SystemExit()
-    
-    
-    print("Hello World!")
-
 
 if __name__ == "__main__":
     main()
